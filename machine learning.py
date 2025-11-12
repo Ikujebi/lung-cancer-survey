0a86485d794f645caee6f26cfb765df65cccdb80
Damilare Igbosanya <138799846+Holymichael99@users.noreply.github.com>
2025-11-12 10:45:50 +0100

    importing the libraries and loading the data

---
+++ machine learning.py
@@ -0,0 +1,11 @@
+# Import libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+# Load the dataset
+df = pd.read_csv("survey lung cancer.csv")
